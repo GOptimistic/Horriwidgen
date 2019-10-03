7575663c1f3f65e402815c408be25037a54bddc7
GOptimistic <969935802@qq.com>
2019-10-03 17:06:33 +0800

    2019-10-3-second

--- miniprogram/pages/mainwindow/rooms/rooms.js
+++ miniprogram/pages/mainwindow/rooms/rooms.js
@@ -56,26 +56,6 @@ Page({
       }
     })
   },
-  getPhoneNumber: function (e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.iv)
-    console.log(e.detail.encryptedData)
-    if (e.detail.errMsg == 'getPhoneNumber:fail user deny') {
-      wx.showModal({
-        title: '提示',
-        showCancel: false,
-        content: '未授权',
-        success: function (res) { }
-      })
-    } else {
-      wx.showModal({
-        title: '提示',
-        showCancel: false,
-        content: '同意授权',
-        success: function (res) { }
-      })
-    }
-  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
