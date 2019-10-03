a8f57bd34099b94227883d4727f8164b8441ab32
marui <marui99@foxmail.com>
2019-10-03 21:22:29 +0800

    只有空壳的轮播图

--- miniprogram/pages/mainwindow/homepage/homepage.js
+++ miniprogram/pages/mainwindow/homepage/homepage.js
@@ -5,62 +5,101 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperCurrent: 0,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 800,
+    circular: true,
+    imgUrls: [
+      '../../images/1.jpg',
+      '../../images/1.jpg',
+      'https://sm.ms/image/UAFDMQPpnqVz8XG',
+      'https://sm.ms/image/UAFDMQPpnqVz8XG'
+    ],
+    links: [
+      '../orders/orders',
+      '../orders/orders',
+      '../orders/orders',
+      '../orders/orders'
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //轮播图的切换事件
+  swiperChange: function(e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
   },
+
+  //点击指示点切换
+  chuangEvent: function(e) {
+    this.setData({
+      swiperCurrent: e.currentTarget.id
+    })
+  },
+
+  //点击图片触发事件
+  swipclick: function(e) {
+    console.log(this.data.swiperCurrent);
+    wx.switchTab({
+      url: this.data.links[this.data.swiperCurrent]
+    })
+  },
+
+/**
+ * 生命周期函数--监听页面加载
+ */
+onLoad: function (options) {
+
+},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
