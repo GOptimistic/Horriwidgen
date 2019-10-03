eb1249168bccc13371aff72c707aecbf81fb3e64
marui <marui99@foxmail.com>
2019-10-03 23:18:54 +0800

    完美的轮播图哈哈哈哈哈哈耶

--- miniprogram/pages/mainwindow/homepage/homepage.js
+++ miniprogram/pages/mainwindow/homepage/homepage.js
@@ -4,49 +4,58 @@ Page({
   /**
    * 页面的初始数据
    */
+  // data: {
+    // swiperCurrent: 0,
+    // indicatorDots: true,
+    // autoplay: true,
+    // interval: 3000,
+    // duration: 800,
+    // circular: true,
+  //   imgUrls: [
+  //     '../../../images/1.jpg',
+  //     '../../../images/1.jpg',
+  //     // 'https://sm.ms/image/UAFDMQPpnqVz8XG',
+  //     // 'https://sm.ms/image/UAFDMQPpnqVz8XG'
+  //   ],
+  //   links: [
+  //     '../orders/orders',
+  //     '../orders/orders',
+  //     // '../orders/orders',
+  //     // '../orders/orders'
+  //   ]
+  // },
   data: {
+    imgUrls: [
+      'http://m.qpic.cn/psb?/V12p8Tks3RZSb7/FP*g4VYkT3Fd4H6Qk28xiXs1dI*5TC3g10Ep4PVH2p4!/b/dDABAAAAAAAA&bo=xQLdAQAAAAARBys!&rf=viewer_4',
+      'http://m.qpic.cn/psb?/V12p8Tks3RZSb7/rgaP4eJ6*Lq5y9j4617VaQD9XA8IBXY1bAJRNA42TBE!/b/dFMBAAAAAAAA&bo=IAPPAQAAAAARB90!&rf=viewer_4',
+    ],
     swiperCurrent: 0,
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 800,
     circular: true,
-    imgUrls: [
-      '../../images/1.jpg',
-      '../../images/1.jpg',
-      'https://sm.ms/image/UAFDMQPpnqVz8XG',
-      'https://sm.ms/image/UAFDMQPpnqVz8XG'
-    ],
-    links: [
-      '../orders/orders',
-      '../orders/orders',
-      '../orders/orders',
-      '../orders/orders'
-    ]
   },
-
-  //轮播图的切换事件
-  swiperChange: function(e) {
-    this.setData({
-      swiperCurrent: e.detail.current
-    })
-  },
-
-  //点击指示点切换
-  chuangEvent: function(e) {
-    this.setData({
-      swiperCurrent: e.currentTarget.id
-    })
-  },
-
-  //点击图片触发事件
-  swipclick: function(e) {
-    console.log(this.data.swiperCurrent);
-    wx.switchTab({
-      url: this.data.links[this.data.swiperCurrent]
-    })
-  },
-
+  // changeIndicatorDots: function (e) {
+  //   this.setData({
+  //     indicatorDots: !this.data.indicatorDots
+  //   })
+  // },
+  // changeAutoplay: function (e) {
+  //   this.setData({
+  //     autoplay: !this.data.autoplay
+  //   })
+  // },
+  // intervalChange: function (e) {
+  //   this.setData({
+  //     interval: e.detail.value
+  //   })
+  // },
+  // durationChange: function (e) {
+  //   this.setData({
+  //     duration: e.detail.value
+  //   })
+  // },
 /**
  * 生命周期函数--监听页面加载
  */
